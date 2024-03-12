de778dc0ae9ba282a6e521d6d040336b9c714386
Dong Liang <ldifer@gmail.com>
2024-03-12 23:21:48 +0100

    Updated version of the data from v3  to v4

--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -1,15 +1,16 @@
-from cybulde.config_schemas.config_schema import Config
+# from cybulde.config_schema.config_schema import Config
 from cybulde.utils.config_utils import get_config
 from cybulde.utils.data_utils import (
     commit_to_dvc,
     initialize_dvc,
     initialize_dvc_storage,
 )
-from cybulde.utils.utils import get_logger
+
+# from cybulde.utils.utils import get_logger
 
 
 @get_config(config_path="../configs", config_name="config")
-def version_data(config: Config) -> None:
+def version_data(config) -> None:
     initialize_dvc()
     initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
     commit_to_dvc(config.dvc_raw_data_folder, config.dvc_remote_name)
